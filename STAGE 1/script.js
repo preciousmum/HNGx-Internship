1d91b6e17c3a4c6be70ac884b39cc6e18caf9068
JEREMIAH JACOB <reginajacobenam@gmail.com>
2023-09-10 21:10:25 +0100

    Updated Timing

--- STAGE 1/script.js
+++ STAGE 1/script.js
@@ -1,17 +1,28 @@
-// script.js
 document.addEventListener("DOMContentLoaded", function () {
     // Get the current date
     const currentDate = new Date();
     
-    
+    // Define an array of days of the week
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     
-    
+    // Find the current day of the week
     const currentDay = daysOfWeek[currentDate.getUTCDay()];
     
-   
-    const currentUTCTime = currentDate.getTime();
+    // Get the current UTC time in milliseconds
+    let currentUTCTime = currentDate.getTime();
     
-    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = `Current Day: ${currentDay}`;
-    document.querySelector('[data-testid="currentUTCTime"]').textContent = `Current UTC Time: ${currentUTCTime}`;
+    
+    const currentDayElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    const currentUTCTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
+    
+    function updateUTCTime() {
+        currentUTCTime += 1; // Increment by 1000 milliseconds (1 second)
+        currentUTCTimeElement.textContent = `Current UTC Time: ${currentUTCTime}`;
+    }
+
+    currentDayElement.textContent = `Current Day: ${currentDay}`;
+    currentUTCTimeElement.textContent = `Current UTC Time: ${currentUTCTime}`;
+
+    
+    setInterval(updateUTCTime, 1);
 });
